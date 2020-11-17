455c6c13c43b47b8dcce947955a551cd9b7f2fdc
hossam-khalaf <hossamhassan303089@gmail.com>
2020-11-17 18:00:47 +0200

    read more && show less buttons

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, price, name }) => {
+  //readmore
+  const [readmore, setReadmore] = useState(false);
+
   return (
     <article className='single-tour'>
       <img src={image} alt={name} />
@@ -9,7 +12,13 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className='tour-price'>${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readmore ? info : `${info.substring(0, 200)}`}
+          <button onClick={() => setReadmore(!readmore)}>
+            {readmore ? 'show less' : 'read more'}
+          </button>
+        </p>
+        <button className='delete-btn'>not intrested</button>
       </footer>
     </article>
   );
