eec33b398161eb7389dbda6159a6325ab1017738
hossam-khalaf <hossamhassan303089@gmail.com>
2020-11-17 16:33:15 +0200

    Loading

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+// Api
+const url = 'https://course-api.com/react-tours-project';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const reponse = await fetch(url);
+    const tours = reponse.json();
+    console.log(tours);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
+}
+
+export default App;
